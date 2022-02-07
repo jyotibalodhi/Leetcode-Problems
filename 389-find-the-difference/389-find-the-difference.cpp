bb5aab32a2b297076cd71d00b2f2253f42e70c55
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-02-07 13:00:53 +0530

    Time: 4 ms (67.41%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> umap1;
-        unordered_map<char, int> umap2;
-        char res;
-        for(int i=0;i<s.length();i++){
-            umap1[s[i]]++;
+       char res=0;
+        for(char c:s) {
+            res^=c;  
         }
-         for(int i=0;i<t.length();i++){
-            umap2[t[i]]++;
-             if(umap2[t[i]]>umap1[t[i]]){
-                  res=t[i];
-                 break;
-             }
+         for(char c:t) {
+            res^=c;      
         }
+        
         return res;
+      
     }
 };
