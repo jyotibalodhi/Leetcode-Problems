29e604bc5c21f61e55ccab2b4e8cdd912b400e08
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-03-18 16:32:36 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (44.89%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> stk;
+        int score=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                stk.push(score);
+                score=0;
+            }
+            else{
+                score =stk.top() + max(2*score, 1);
+                stk.pop();
+            }
+        }
+        return score;
+    }
+};
