1eb82047bc748e79af8024463c999ebf0078a396
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-03-23 11:46:32 +0530

    Time: 5 ms (16.41%), Space: 6 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int brokenCalc(int x, int y) {
-        int ans=0;
-        
-        while(x!=y){
-            if(y<=x){
-                ans = ans + (x-y); 
-                break;
-            }
-            else{
-                if(y % 2!=0) {
-                    y++;
-                    ans++;
-                }
-                y = y/2;
-                ans++;
-        }
-       }
+        int res = 0;
+        while (x< y) {
             
-    return ans;
+            if (y % 2) 
+                y++;
+            
+            else 
+                y /= 2;
+            
+            res++;
+        }
+        return res + x - y;
     }
 };
