b9eea6fca39b56ae9cb040c054351b1a56e03e1d
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-04-12 20:41:04 +0530

    Time: 3 ms (29.75%), Space: 6.5 MB (71.43%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,10 +4,9 @@ public:
         
         vector<vector<int>> res(n);
         
-        for (int i = 0 ; i < n ; i++) {
-           res[i].resize(i+1, 1);
-        }
+        
         for(int i=0;i<n;i++){
+              res[i].resize(i+1, 1);
             for(int j=0;j<i;j++){
                 if(j==0 ){
                     continue;
