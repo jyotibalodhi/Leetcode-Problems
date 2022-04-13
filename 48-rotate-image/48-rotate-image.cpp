7990fac1145f6a7a7c89d570236eb80ffd93c4fe
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-04-13 11:41:03 +0530

    Time: 2 ms (64.62%), Space: 7.1 MB (33.01%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
         
-        int n= mat.size();
-        int size= pow(n,2);
-        vector<int> val(size);
+        int n= mat.size(); 
         
-        int k=0;
-        
+        //transpose
         for( int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                val[k++] = mat[i][j];
+            for(int j=0;j<i;j++){
+                if(i==j) continue;
+                swap(mat[i][j],mat[j][i]);
             }
         }
+    
+        //reversing every row
         
-        k=0;
-          for( int i=n-1;i>=0;i--){
-            for(int j=0;j<n;j++){
-                mat[j][i]=val[k++];
-            }
-        }
+         for( int i=0;i<n;i++){         
+            reverse(mat[i].begin(),mat[i].end());
+         }
+        
+     
     }
 };
