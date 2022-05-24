52b9d8fa5307290abe6dc6e7603f0d0bfbc77ffc
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-05-24 22:28:48 +0530

    Time: 3 ms (79.81%), Space: 7.7 MB (56.28%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     
-    void solve(vector<int>& nums, set<vector<int>>& ans,vector<int> ds,int i){
-        if(i==nums.size()){
-            sort(ds.begin(),ds.end());
-            ans.insert(ds);
-            return;
+    void solve(vector<int>& nums, vector<vector<int>>& ans,vector<int> ds,int ind){
+       ans.push_back(ds);
+        
+        for(int i=ind ;i <nums.size();i++){
+            //for avoiding duplicates
+            if(i!=ind && nums[i]== nums[i-1]) continue;
+            
+            ds.push_back(nums[i]);
+            solve(nums,ans,ds,i+1);
+            ds.pop_back();
         }
-        
-        ds.push_back(nums[i]);
-        solve(nums,ans,ds,i+1);
-        
-        ds.pop_back();
-        solve(nums,ans,ds ,i+1);
 
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> ds;
+        
+        sort(nums.begin(),nums.end());
         
         solve(nums,ans, ds, 0);
         
-      vector<vector<int>> res(ans.begin(),ans.end());
-        return res;
+        return ans;
     }
 };
