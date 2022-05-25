3183a225ad7a07fb3a0ef11c3552dbfda745945d
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-05-25 11:19:36 +0530

    Time: 12 ms (38.17%), Space: 11.5 MB (29.86%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -3,13 +3,16 @@ public:
     
     void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> ds, int t, int ind){
       
-            if(t==0)
+            if(t==0){
                 ans.push_back(ds);
-
+                return;
+            }
+        
         for(int i=ind ;i<nums.size();i++){
             if(i!=ind && nums[i]==nums[i-1]) continue;
             
             if(nums[i] >t) break;
+            
             ds.push_back(nums[i]);
             solve(nums,ans,ds,t-nums[i],i+1);
             ds.pop_back();
