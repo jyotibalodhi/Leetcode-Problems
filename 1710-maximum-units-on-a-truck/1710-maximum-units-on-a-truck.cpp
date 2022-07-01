bc67f6805015b81ed40f97aacb9168778ab9e617
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-01 11:57:40 +0530

    Time: 54 ms (75.43%), Space: 16 MB (77.60%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -9,12 +9,9 @@ public:
         
         sort(arr.begin(),arr.end(),compare);
 
-        
-        int boxes=0;
         int units=0;
 
-        for(int i=0;i<arr.size();i++){
-            
+        for(int i=0;i<arr.size();i++){ 
                 int numBoxes = min(size, arr[i][0]);
                 units += numBoxes * arr[i][1];
                 size -= numBoxes;
