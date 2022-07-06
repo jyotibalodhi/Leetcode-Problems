00871e777e535863328b98d15854c149d3fb175f
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-06 11:05:43 +0530

    Time: 29 ms (65.48%), Space: 14 MB (60.49%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<int> findOrder(int v, vector<vector<int>>& arr) {
         
+        //we need to find reverse topological sort 
+        // and if cycle present return empty array
         
         unordered_map<int, vector<int>> adj;
         vector<int> ind(v,0);
@@ -9,9 +11,9 @@ public:
         
         vector<int> ans;
         
-        //making adjancy list
+        //making reverse adjancy list
         for(int i=0;i<arr.size();i++){
-            adj[arr[i][0]].push_back(arr[i][1]);
+            adj[arr[i][1]].push_back(arr[i][0]);
         }
         
         
@@ -42,7 +44,7 @@ public:
         }
         
         if(cnt != v) return {};    // if cyclic graph
-        reverse(ans.begin(),ans.end());
+
         return ans;
        
     }
