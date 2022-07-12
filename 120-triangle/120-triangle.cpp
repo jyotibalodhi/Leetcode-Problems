b07e6f4c0479c1923f4af2065fb409eb3544fa01
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-12 18:26:25 +0530

    Time: 0 ms (100.00%), Space: 9 MB (22.47%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,25 +1,30 @@
 class Solution {
-public:
-   int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
-       
-		if(i == n - 1)
-			return triangle[i][j];
-       
-		if(dp[i][j] != -1)
-			return dp[i][j];
-       
-		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
-		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
-       
-		return dp[i][j] = min(bottom, bottom_right);
-	}
+public:  
     
+	int minimumTotal(vector<vector<int>>& tri) {
+        
+    int n = tri.size();
+        
+	vector<vector<int>> dp(n, vector<int>(n,0));
     
-	int minimumTotal(vector<vector<int>>& triangle) {
         
-		int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -1));
+        //base case Tabulation: n-1 ---> 0
         
-		return solve(0, 0, n, triangle, dp);
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = tri[n-1][j];
+        }
+        
+        for( int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){ 
+                
+                   int bottom = tri[i][j] + dp[i+1][j];
+                    int bottomRight = tri[i][j] + dp[i+1][j+1];
+                    
+                    dp[i][j] = min(bottom, bottomRight);
+                }
+        }
+    return dp[0][0];
+        
+        
 	}
 };
