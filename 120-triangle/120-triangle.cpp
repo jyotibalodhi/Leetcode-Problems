0e4132c5d23c797166a7df5ac97d9dd75b62a115
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-12 18:32:16 +0530

    Time: 17 ms (11.16%), Space: 8.9 MB (43.40%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -5,25 +5,30 @@ public:
         
     int n = tri.size();
         
-	vector<vector<int>> dp(n, vector<int>(n,0));
+	// vector<vector<int>> dp(n, vector<int>(n,0));
     
         
         //base case Tabulation: n-1 ---> 0
         
+        vector<int> prev(n,0);
+        
         for(int j=0;j<n;j++){
-            dp[n-1][j] = tri[n-1][j];
+            prev[j] = tri[n-1][j];
         }
         
         for( int i=n-2;i>=0;i--){
+            vector<int> curr(i+1,0);
+            
             for(int j=i;j>=0;j--){ 
                 
-                   int bottom = tri[i][j] + dp[i+1][j];
-                    int bottomRight = tri[i][j] + dp[i+1][j+1];
+                   int bottom = tri[i][j] + prev[j];
+                   int bottomRight = tri[i][j] + prev[j+1];
                     
-                    dp[i][j] = min(bottom, bottomRight);
+                    curr[j] = min(bottom, bottomRight);
                 }
+            prev = curr;
         }
-    return dp[0][0];
+    return prev[0];
         
         
 	}
