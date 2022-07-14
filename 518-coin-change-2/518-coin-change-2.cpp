a0ed0176c2366b0f889a0547a2f1ac0f5c915339
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-14 19:33:38 +0530

    Time: 106 ms (12.01%), Space: 18.4 MB (42.13%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-        
-int solve(vector<int> &nums, int x, int i, vector<vector<int>> &dp){
-        if(i<0)  return 0;      
-    
-        if(x==0) return 1;
-        if(i==0){
-            if(x % nums[i] == 0) return 1;
-            return 0;
-        }
-
-        if(dp[i][x] !=-1) return dp[i][x];
-
-        int take = 0;
-        if(x>= nums[i]){
-            take = solve(nums, x - nums[i], i ,dp);
-        }
-        int notTake = solve(nums, x, i-1,dp );
-
-        return dp[i][x]= take + notTake;
-}
-
     
     int change(int x, vector<int>& nums) {
           
         int n = nums.size();
         
-        vector<vector<int>> dp(n,vector<int>(x+1,-1));
+        vector<vector<int>> dp(n,vector<int>(x+1,0));
         
-        return solve(nums,x,n-1,dp);
+        for(int i=0;i<=x;i++){
+            if( i%nums[0]==0) 
+                dp[0][i] = 1;
+        }
+
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=x;j++){
+                int take = 0;
+                
+                if(j>= nums[i]){
+                    take = dp[i][j- nums[i]];
+                }
+                int notTake = dp[i-1][j];
+
+                dp[i][j]= take + notTake;
+            }
+        }
+        
+        return dp[n-1][x];
         
     }
 };
