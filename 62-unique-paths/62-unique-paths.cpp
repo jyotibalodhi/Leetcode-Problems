ed41ae5a2bc2481e1b3cbe38b24c017e7c56d7a9
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-15 22:48:48 +0530

    Time: 5 ms (18.38%), Space: 6 MB (73.52%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,27 +5,14 @@ public:
         
         if(m==1 && n==1) return 1;
         
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        int N = m+n -2;
+        int r = min(m-1,n-1);
         
-        dp[0][0] =1;
-        
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if( i==0 && j ==0) dp[0][0] =1;
-                else{
-                  int up = 0;
-                  if(i>0)
-                    up += dp[i-1][j];
-                
-                  int down = 0;
-                  if(j>0)
-                    down += dp[i][j-1];
-
-                 dp[i][j] = up + down;
-                }
-            }
+        double res =1.0;
+        for(int i=1;i<=r;i++){
+            res = res * (N-r +i)/i;
         }
-        return dp[m-1][n-1];
+        
+        return (int)res;
     }
 };
