4c1db4a6d06e8912197710f62556be4298051637
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-07-24 11:59:15 +0530

    Time: 269 ms (21.68%), Space: 15 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -3,28 +3,21 @@ public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         
         //Binary Search approach as every row and column are sorted
-        int m = mat.size();
+         int m = mat.size();
         int n = mat[0].size();
         
+        int i = m - 1;
+        int j = 0;
         
-        for(int i=0;i<m;i++){
-            if(mat[i][0] > target || mat[i][n-1] <target) continue;
+        while (i>=0 && j<n){
             
-            int l =0, h= n-1;
-            vector<int> row = mat[i];
+            if (mat[i][j] == target) return true;
             
-            while(l<=h){
-                int m = l+(h-l)/2;
-                
-                if(row[m] == target) 
-                    return true;
-                else if(row[m] < target)
-                    l = m+1;
-                else
-                    h= m-1;
-            }
+            else if (mat[i][j] < target) j++;
+            
+            else i--;
         }
-        
         return false;
+        
     }
 };
