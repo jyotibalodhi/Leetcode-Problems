f80c771215783c86b9d2619d9debed756268a553
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-08-01 20:52:25 +0530

    Time: 3 ms (74.23%), Space: 7 MB (87.47%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     
-    void solve(vector<int>& nums, vector<vector<int>> &ans, vector<int> ds, int ind){
+//     void solve(vector<int>& nums, vector<vector<int>> &ans, vector<int> ds, int ind){
         
-       if(ind == nums.size()){
-           ans.push_back(ds);
-           return;
-       }
+//        if(ind == nums.size()){
+//            ans.push_back(ds);
+//            return;
+//        }
         
         
-        ds.push_back(nums[ind]);  //inculde
-        solve(nums,ans,ds,ind+1);
+//         ds.push_back(nums[ind]);  //inculde
+//         solve(nums,ans,ds,ind+1);
         
-        //exclude
+//         //exclude
         
-        ds.pop_back();
-        solve(nums,ans,ds,ind+1);
-    }
+//         ds.pop_back();
+//         solve(nums,ans,ds,ind+1);
+//     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
+//         vector<int> ds;
         
-        solve(nums,ans, ds,0);
+//         solve(nums,ans, ds,0);
+        
+//         return ans;
+        
+        int n = nums.size();
+        
+        for(int i=0;i<(1<<n);i++)  //i--> 2^n -1
+        {
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j))
+                    temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+        }
         
         return ans;
     }
