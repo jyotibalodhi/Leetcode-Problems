1caf1b8565506c302912fb6346ea119da5004482
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-08-02 10:53:17 +0530

    Time: 53 ms (52.59%), Space: 13 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,18 +2,47 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& mat, int k) {
         
-        priority_queue<int, vector<int>> pq;  //max heap
-        int n = mat.size();
+//         priority_queue<int, vector<int>> pq;  //max heap
+//         int n = mat.size();
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                pq.push(mat[i][j]);
-                if(pq.size() >k){
-                    pq.pop();
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 pq.push(mat[i][j]);
+//                 if(pq.size() >k){
+//                     pq.pop();
+//                 }
+//             }
+//         }
+        
+//         return pq.top();
+        
+        int n = mat.size();
+        int count =0,temp;
+        
+        int low = mat[0][0];  //smallest element
+        int high = mat[n-1][n-1];  // largest element
+        
+        while(low < high){
+            int mid = low + (high - low)/2;
+            
+             temp = n - 1;
+             count = 0;
+            
+            // For each row count the elements that are smaller than mid
+            for(int i = 0; i < n; i++){
+                
+                while(temp >= 0 && mat[i][temp] > mid){
+                    temp--;
                 }
+                count+= (temp+1);
             }
+            
+            if(count <k) low = mid +1;  //need more larger elements
+            else 
+                high = mid;
+            
         }
         
-        return pq.top();
+        return low;
     }
 };
