9630eecafde94d4b61cf88887185c8a67526e93d
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-08-11 17:20:46 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.78%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,44 +1,68 @@
 class Solution {
+public:
     
-    void solve(vector<string> &board,vector<vector<string>> &ans, vector<int> lr, vector<int> ud, vector<int> ld, int col, int n){
+//     bool isSafe(vector<string> &board,int r, int c, int n){
         
+//         for(int i=0;i<c;i++){
+//             if(board[r][i] == 'Q')   //queen in same row
+//                 return false;
+//         }
         
-        if(col==n){
+//         int i =r,j=c;
+//         //upper diagonal
+//         while(i>=0 && j>=0){
+//             if(board[i][j] =='Q')
+//                 return false;
+//             i--;
+//             j--;
+//         }
+        
+//         i=r,j=c;
+        
+//         //lower diagonal
+        
+//         while(i<n && j>=0){
+//             if(board[i][j] =='Q')
+//                 return false;
+//             i++;
+//             j--;
+//         }
+        
+//         return true;
+//     }
+    
+    void solve(vector<string>&board, vector<vector<string>> &ans, int n, int col,vector<int> &lr, vector<int> &ud, vector<int> &ld){
+        
+        if(col == n){
             ans.push_back(board);
-            return ;
+            return;
         }
         
-        for(int row=0;row<n;row++){
-            if(lr[row] ==0 && ud[row+col] ==0 && ld[n-1+col-row]==0){
-                lr[row]=1;
+        for(int row =0; row<n ; row++){
+            // if(isSafe(board,row,col,n)){
+            if(lr[row] ==0 && ud[row+col]==0 && ld[n-1+col-row]==0){
+                board[row][col] = 'Q';
+                lr[row] =1;
                 ud[row+col] =1;
-                ld[n-1+col-row]=1;
-                board[row][col]='Q';
-                
-                solve(board, ans, lr, ud,ld,col+1,n);
-                
-                 lr[row]=0;
+                ld[n-1+col-row] =1;
+                solve(board,ans,n,col+1,lr,ud,ld);
+                lr[row] =0;
                 ud[row+col] =0;
-                ld[n-1+col-row]=0;
-                board[row][col]='.';
+                ld[n-1+col-row] =0;
+                board[row][col] ='.';
             }
         }
     }
-public:
     vector<vector<string>> solveNQueens(int n) {
         
+        vector<vector<string>> ans;
         string s(n,'.');
+        
         vector<string> board(n,s);
         
-        vector<vector<string>> ans;
+        vector<int> lR(n,0),uD(2*n -1,0),lD(2*n -1,0);
         
-        
-        vector<int> lr(n,0);  // left row
-        vector<int> ud(2*n-1,0); // upper diagonal
-        vector<int> ld(2*n-1,0); //lower diagonal
-
-
-        solve(board, ans, lr, ud,ld,0,n);
+        solve(board,ans,n,0,lR,uD,lD);
         return ans;
     }
 };
