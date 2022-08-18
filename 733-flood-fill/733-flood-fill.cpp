ec9e536b1a93ba761bc96d1699eabfeb6232f717
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-08-18 11:11:07 +0530

    Time: 11 ms (80.56%), Space: 14 MB (78.54%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-   void dfs(vector<vector<int>>& image, int i, int j, int color, int parColor) {
-        if(i<0 || i>=image.size() || j<0 || j>=image[0].size()) return;
-        if(image[i][j]==parColor) {
-            image[i][j]=color;
-            
-            
-            dfs(image, i-1, j, color, parColor);
-            dfs(image, i+1, j, color, parColor);
-            dfs(image, i, j-1, color, parColor);
-            dfs(image, i, j+1, color, parColor);
+    void solve(vector<vector<int>>& image, int i, int j, int prevCol, int newCol){
+        
+        if(i <0 || i>=image.size() || j<0 || j>= image[0].size()){
+            return;
         }
-        return;
+        
+        if(image[i][j] == prevCol){
+            image[i][j] = newCol;
+        
+            solve(image, i+1,j,prevCol, newCol);  // down
+            solve(image, i,j+1,prevCol, newCol);  // right
+            solve(image, i-1,j,prevCol, newCol);  // up
+            solve(image, i,j-1,prevCol, newCol);  // left
+        }
+        
     }
     
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc] == color) return image;
-        dfs(image, sr, sc, color, image[sr][sc]);
+        int col = image[sr][sc];
+        if(color == col)   // no change needed
+            return image;
+        
+        solve(image, sr,sc,col, color);
         return image;
     }
 };
