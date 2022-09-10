609c0b1abb78ecdf7ad126ad2925b51a3780493e
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-09-10 11:28:53 +0530

    Time: 0 ms (100.00%), Space: 13 MB (79.66%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,39 +1,64 @@
+#define pii pair<int, int>
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-     
         
-         if(grid.empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), days = 0, tot = 0, cnt = 0;
-        queue<pair<int, int>> rotten;
         
-        //calculating total oranges and pushing rotten one's in queue
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(grid[i][j] != 0) tot++;
-                if(grid[i][j] == 2) rotten.push({i, j});
-            }
-        }
+        int m = grid.size();
+        int n = grid[0].size();
         
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
+        int total =0;       // Total oranges
+        int count =0;       // Rotten oranges
         
-        while(!rotten.empty()){
-            int k = rotten.size();
-            cnt += k; 
-            while(k--){
-                int x = rotten.front().first, y = rotten.front().second;
-                rotten.pop();
-                for(int i = 0; i < 4; ++i){
-                    int nx = x + dx[i], ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1) continue;
-                    grid[nx][ny] = 2;
-                    rotten.push({nx, ny});
+        queue<pii> q;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] != 0)
+                    total++;
+                if(grid[i][j] == 2){
+                    count++;
+                    q.push({i,j});
                 }
             }
-            if(!rotten.empty()) days++;
         }
         
-        return tot == cnt ? days : -1;
+        int time = 0;
+        
+        int dx[4] = {1,0,-1,0};    //right,down,left,up
+        int dy[4] = {0,-1,0,1};
+        
+        while(!q.empty()){
+            
+            int k = q.size();
+            
+            
+            while(k--){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+
+                for(int i=0;i<4;i++){     // checking all four directions
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if(nx >= 0 && ny >=0 && nx < m && ny <n){
+                        if(grid[nx][ny] == 1){
+                            grid[nx][ny] = 2;
+                            q.push({nx,ny});
+                            count++;
+                        }
+                    }
+                }
+            }
+            if(!q.empty())
+                time++;
+        }
+        
+        if(total==count)
+            return time;
+        
+        return -1;
     }
 };
