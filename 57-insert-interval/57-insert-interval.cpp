a2a96961d0542045b439d3d80bf7287779f35383
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-09-17 13:54:22 +0530

    Time: 41 ms (18.54%), Space: 17.1 MB (74.66%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newnums) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        int n = nums.size();
+         int n = intervals.size()-1;
+        vector<vector<int>> res;
         
-        if(n == 0)
-            return {newnums};
-        
-        vector<vector<int>> ans;
-        
-        nums.push_back(newnums);
-        
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<=n;i++){
-            
-            vector<int> interval = nums[i];
-            
-            if(ans.empty())  //first interval
-            {
-                ans.push_back(interval);
+        for(int i = 0; i < intervals.size(); i++){
+            if(intervals[i][1] < newInterval[0]){
+                res.push_back(intervals[i]);
             }
-            else{
-                vector<int> temp = ans.back();
-                
-                if(interval[0] > temp[1])        // non overlapping interval
-                {
-                    ans.push_back(interval);
-                }
-                else{                             // over lapping interval
-                    
-                    int val = max(interval[1],temp[1]);
-                    temp[1] = val;
-                    ans.pop_back();
-                    ans.push_back(temp);
-                }
+            
+            else if(intervals[i][0] > newInterval[1]){
+                res.push_back(newInterval);
+                newInterval = intervals[i];
+            }
+            
+            else if(intervals[i][1] >= newInterval[0] || intervals[i][0] <= newInterval[1]){
+                newInterval[0] = min(intervals[i][0], newInterval[0]);
+                newInterval[1] = max(intervals[i][1], newInterval[1]);
             }
         }
-        
-        return ans;
+       res.push_back(newInterval);
+       return res;
     }
 };
