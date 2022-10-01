3a960e78bfdaa42fa1f9c09d081eb3c2df6465c6
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-10-01 18:16:25 +0530

    Time: 104 ms (80.31%), Space: 49.5 MB (11.42%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int f(vector<int> &nums, int targ, int i, vector<vector<long long int>> &dp)
-    {
-        if (i == 0)
-        {
-            if (targ % nums[i] == 0)
-                return dp[i][targ]= targ / nums[i];
-            else
-                return 1e9;
+    int solve(vector<int> &nums, int amount, int i, vector<vector<int>> &dp){
+        
+        //base case
+
+        if(i==0){
+            if(amount % nums[i] == 0)
+                return amount/nums[i];
+            return 1e9;
         }
-
-        if(dp[i][targ]!=-1) return dp[i][targ];
-
-        int not_take=0+f(nums,targ,i-1,dp);
-        int take=INT_MAX;
-
-
-       if(nums[i]<=targ) 
-       {
-           take=1+f(nums,targ-nums[i],i,dp);
-       }
-
-       return dp[i][targ]= min(take,not_take);
+        
+        if(dp[i][amount] != -1)
+            return dp[i][amount];
+        
+        int take = 1e9;
+        if(nums[i] <= amount)
+            take = 1 + solve(nums, amount-nums[i],i,dp);
+        
+        int notTake = solve(nums, amount, i-1, dp);
+        
+        return dp[i][amount] = min(take,notTake);
     }
-
-    int coinChange(vector<int> &nums, int target)
-    {
-        int n = nums.size();
-        vector<vector<long long int>> dp(n+1,vector<long long int> (target+1,-1));
-        long long int ans= f(nums, target, n - 1,dp);
-        if(ans>=1e9) return -1;
+    
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int n = coins.size();
+        
+        vector<vector<int>> dp(n+1, vector<int> (amount+1,-1));
+        
+        int ans = solve(coins, amount, n-1,dp);
+        if(ans >= 1e9)
+            return -1;
         return ans;
     }
 };
