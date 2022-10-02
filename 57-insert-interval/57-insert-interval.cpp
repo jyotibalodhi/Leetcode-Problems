fd28ae72f02e124280a18b5556961a91fcfee925
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-10-02 10:39:32 +0530

    Time: 36 ms (32.94%), Space: 16.9 MB (99.33%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& inter, vector<int>& ni) {
+    vector<vector<int>> insert(vector<vector<int>>& inter, vector<int>& newInter) {
         
         int n = inter.size();
         
-        vector<vector<int>> ans;
+        vector<vector<int>> res;
+        
         
         for(int i=0;i<n;i++){
             
-            if(inter[i][1] < ni[0]){
-                ans.push_back(inter[i]);
+            if(inter[i][1]< newInter[0]){          // newInter strictly > inter
+                res.push_back(inter[i]);
             }
-            else if(inter[i][0] > ni[1]){
-                ans.push_back(ni);
-                ni = inter[i];
+            else if(inter[i][0] > newInter[1]){    // newInter strictly < inter
+                res.push_back(newInter);
+                newInter = inter[i];
             }
-            // else if(inter[i][1] >= ni[0] || inter[i][0] <= ni[0]){
-            else{
-                ni[0] = min(ni[0],inter[i][0]);
-                ni[1] = max(ni[1],inter[i][1]);
+            else{                                  // overlapping intervals
+                newInter[0] = min(newInter[0], inter[i][0]);
+                newInter[1] = max(newInter[1], inter[i][1]);
             }
         }
         
-        ans.push_back(ni);
-        return ans;
+        res.push_back(newInter);
+        return res;
     }
 };
