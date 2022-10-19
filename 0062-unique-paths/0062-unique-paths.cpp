246c4ec2cce52934cb2be4e9a92cd315e4b44bd6
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-10-19 12:56:57 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (28.81%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    int solve(int row, int col, vector<vector<int>> &dp){
+        
+        
+        if(row == 0 && col ==0){
+            return 1;
+        }
+        
+        if(row < 0 || col < 0){
+            return 0;
+        }
+        
+        if(dp[row][col] != -1)
+            return dp[row][col];
+        
+        int up = solve(row-1,col,dp);
+        int left = solve(row, col-1,dp);
+        
+        return dp[row][col] = up + left;
+    }
+    int uniquePaths(int m, int n) {
+        
+        if(m==1 && n ==1)
+            return 1;
+        
+        vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
+        
+        return solve(m-1,n-1,dp);
+    }
+};
