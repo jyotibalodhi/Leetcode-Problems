1409a44392bcd36e324f85c6a3128f48c50ae4cb
Jyoti Balodhi <63946672+jyotibalodhi@users.noreply.github.com>
2022-10-19 12:59:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (91.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
     
-    int solve(int row, int col, vector<vector<int>> &dp){
+//     int solve(int row, int col, vector<vector<int>> &dp){
         
         
-        if(row == 0 && col ==0){
-            return 1;
-        }
+//         if(row == 0 && col ==0){
+//             return 1;
+//         }
         
-        if(row < 0 || col < 0){
-            return 0;
-        }
+//         if(row < 0 || col < 0){
+//             return 0;
+//         }
         
-        if(dp[row][col] != -1)
-            return dp[row][col];
+//         if(dp[row][col] != -1)
+//             return dp[row][col];
         
-        int up = solve(row-1,col,dp);
-        int left = solve(row, col-1,dp);
+//         int up = solve(row-1,col,dp);
+//         int left = solve(row, col-1,dp);
         
-        return dp[row][col] = up + left;
-    }
+//         return dp[row][col] = up + left;
+//     }
     int uniquePaths(int m, int n) {
         
-        if(m==1 && n ==1)
-            return 1;
+//         if(m==1 && n ==1)
+//             return 1;
         
-        vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
+//         vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
         
-        return solve(m-1,n-1,dp);
+//         return solve(m-1,n-1,dp);
+        
+        
+        int N = (m-1) + (n-1);       //total ways
+        int r = min(m-1,n-1);        // fixing one element
+        
+        double ans = 1.0;
+        
+        for(int i=1;i<=r;i++){
+            ans = ans * (N-r+i)/i;
+        }
+        
+        return ans;
     }
 };
